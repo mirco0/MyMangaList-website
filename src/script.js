5448c274fe4f82a2bfb0079f1992ea1c8819a6c9
mirco0 <>
2022-03-13 23:52:33 +0100

    added DataSaverFeature

--- src/script.js
+++ src/script.js
@@ -1,21 +1,35 @@
 window.onload = init;
+window.onbeforeunload = function (e) {
+  if(isDataChanged()){
+    e = e || window.event;
+    if (e) {
+        e.returnValue = ' ';
+    }
+    return ' ';
+  }
+};
+
 let data = [];
 
 function init(){
     initUI();
-    loadData("");
+    loadData();
     generateTable(data.length);
 }
+
 function initUI(){
   var title = window.location.hash.substring(1);
-  document.getElementById("title").innerHTML = title;
-
+  document.getElementById("title").textContent = title.replaceAll("%20"," ");
 }
-function loadData(datast){
+
+function loadData(){
+  const datast = sessionStorage.getItem("data");
+  console.log(datast);
   for(let i = 0; i<datast.length; i++){
     data[i] = ~~datast.charAt(i);
   }
 }
+
 function saveData(){}
 function toggleItem(object,index){
   data[index] = !data[index];
@@ -26,14 +40,19 @@ function setColor(object,index){
   data[index] == 1? color = "--selected_dark" : color = "--unselected_dark";
   object.style.background = window.getComputedStyle(document.documentElement).getPropertyValue(color);
 }
+
 function generateTable(cols){
     for (let i = 1; i <=cols; i++) {
         var object = document.createElement("div");
         object.className = "grid-item"
-        object.innerHTML = i;
+        object.textContent = i;
         setColor(object,i-1);
-        object.addEventListener('click', function(){toggleItem(this,this.innerHTML-1);});
+        object.addEventListener('click', function(){toggleItem(this,this.textContent-1);});
         var element = document.getElementById("grid");
         element.appendChild(object);
     }
+}
+
+function isDataChanged(){
+  return sessionStorage.getItem("data") === data;
 }
