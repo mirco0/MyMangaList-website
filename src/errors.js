6d50d5a6d593173e80a18122140054f5b95cbe4b
mirco0 <>
2022-09-26 18:14:08 +0200

    added loading animation and error

---
+++ src/errors.js
@@ -0,0 +1,10 @@
+export const errors = {
+    'auth/internal-error' : 'The Authentication server encountered an unexpected error',
+    'auth/invalid-email' : 'The provided value for your email is invalid',
+    'auth/invalid-password' : 'The provided value for your password is invalid',
+    'auth/weak-password' : 'The provided password is too weak (less than 6 characters)',
+    'auth/wrong-password' : 'The email and password you entered don\'t match',
+    'auth/too-many-requests':'Troppe richieste inviate al server',
+    'auth/user-not-found' : 'No user corresponding to this email',
+    'auth/email-already-in-use':'The email is already in use'
+}
--- src/login.js
+++ src/login.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
-
+import { errors } from "./errors.js";
 
 document.getElementById("logo").onclick = function(){
     window.location.href = "index.html";
 }
-
-document.getElementById("form").onsubmit = function(email,password){ return login(this.elements["email"].value,this.elements["psw"].value);}
-
+document.getElementById("create").onclick = function(){
+  window.location.href = "signin.html"
+};
+document.getElementById("form").onsubmit = function(){ return login(this.elements["email"].value,this.elements["psw"].value);}
 const firebaseConfig = initializeApp({
     apiKey: "AIzaSyCQro-kkRNm58eDJeJgSea3gTj6Q__QzxM",
     authDomain: "mymanga-list.firebaseapp.com",
@@ -19,17 +20,43 @@ const firebaseConfig = initializeApp({
 const auth = getAuth(firebaseConfig);
 
 function login(email,password){
+    animate(true);
+    setError("");
     signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
         const user = userCredential.user;
         console.log(user.uid);
         window.location.href = "list.html";
+        animate(false);
         return true;
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
+        console.log(error.code);
+        setError(errors[error.code]);
+        animate(false);
         return false;
       });
       return false;
+}
+
+function animate(animating){
+  let loader = document.getElementById("loader");
+  let button = document.getElementById("button-login");
+  
+  if(animating){
+    loader.style.display = "block";
+    button.style.fontSize = 0;
+  }else{
+    loader.style.display = "none";
+    button.style.fontSize = "140%";
+  }
+}
+
+function setError(text){
+  let error = document.getElementById("error"); 
+  if(text === ""){
+    error.style.display = "none";
+  }else{
+    error.style.display = "block";
+    error.innerHTML = text;
+  }
 }
